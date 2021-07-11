cb8b7a2b0dfc6d8286336d35166f578963d19058
HaoWang95 <alanwang0028@gmail.com>
2021-07-11 23:49:20 +1000

    angular list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template:
     `<div>
-        <h1>{{title}}</h1>
+        <cp-itemlist></cp-itemlist>
     </div>`
 })
 export class AppComponent {
---
+++ src/app/items/item-list.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'cp-itemlist',
+    templateUrl:'./item-list.component.html',
+})
+export class ItemListComponent {
+
+}

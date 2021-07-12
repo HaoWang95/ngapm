e19c7617153521e37a981d9d79187d1d255b4108
HaoWang95 <alanwang0028@gmail.com>
2021-07-12 11:07:23 +1000

    ng pipes

--- src/app/items/item-list.component.ts
+++ src/app/items/item-list.component.ts
@@ -1,10 +1,39 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'cp-itemlist',
-    templateUrl:'./item-list.component.html',
+    selector: 'cp-itemlist',
+    templateUrl: './item-list.component.html',
 })
 export class ItemListComponent {
     description: string = "My list";
-    items: any[] = [];
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    filterBy: string = 'default';
+    items: any[] = [
+        {
+            "productId": 1,
+            "productName": "Computer system, a programmer's perspective",
+            "productCode": "ISBN-XXX",
+            "releaseDate": "March 18,2019",
+            "description": "The book that you must read to be a programmer",
+            "price": 80,
+            "rating": 5,
+            "imageUrl": "",
+        },
+        {
+            "productId": 5,
+            "productName": "High Performance MySql",
+            "productCode": "ISBN-XXX",
+            "releaseDate": "March 18,2019",
+            "description": "The book that you must read to be a programmer",
+            "price": 80,
+            "rating": 5,
+            "imageUrl": "",
+        }
+    ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 }

019f5675394eb7e4fafc07c6655fd33c6ba08dd5
HaoWang95 <alanwang0028@gmail.com>
2021-07-13 11:56:27 +1000

    common routing

---
+++ src/app/items/item-detail/item-detail.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemDetailGuard implements CanActivate {
+
+  constructor(private router: Router){
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const id = Number(route.paramMap.get('id'));
+      if(isNaN(id) || id < 1){
+        alert('Ivalid item id');
+        this.router.navigate(['/items']);
+        return false;
+      }
+    return true;
+  }
+  
+}
